676c6744532f7b99989eb794bc946a9800895ddb
Jesse Abuaja <jesseabuaja@hotmail.com>
2023-03-30 04:42:36 -0700

    1-strncat complete.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,27 @@
  **/
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len;
-	int src_len;
+	int dest_len, src_len;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	if (n <= 0)
 	{
+		return (dest);
 	}
-	for (src_len = 0; src_len < n; src_len++)
+
+	dest_len = 0;
+	src_len = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	for (src_len = 0; src_len < n && src[src_len] != '\0'; src_len++)
 	{
 		dest[dest_len + src_len] = src[src_len];
 	}
 
+	dest[dest_len + src_len + 1] = '\0';
+
 	return (dest);
+
 }
