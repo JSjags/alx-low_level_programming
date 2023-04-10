ff5420dc6f9c9232c07946fbcf544798d840150a
JSjags <jesseabuaja@hotmail.com>
2023-04-10 10:14:35 +0100

    3-puts.c fixed

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,13 @@
 #include "main.h"
+void _puts(char *str)
 #include<stdio.h>
 /**
- * _puts - Entry point
- * @s: string
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-void _puts(char *s)
+int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
