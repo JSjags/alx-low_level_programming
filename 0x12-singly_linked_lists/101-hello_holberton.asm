0c6d58d9cda7bf564ef6ba42af7c66734c8d0e1f
JSjags <jesseabuaja@hotmail.com>
2023-04-28 16:40:18 +0100

    Project done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,19 @@
-		global	main
+section .data
+    hello db "Hello, Holberton", 0
+    fmt db "%s\n", 0
 
-		section	.text
-main:	mov	rax, 1
-		mov	rdi, 1
-		mov	rsi, holberton
-		mov	rdx, 17
-		syscall
-		mov	rax, 60
-		xor	rdi, rdi
-		syscall
+section .text
+    global main
 
-		section	.data
-holberton:db	"Hello, Holberton", 10
+main:
+    push rbp
+    mov rbp, rsp
+
+    push hello
+    push fmt
+    call printf
+    add rsp, 16
+
+    mov eax, 0
+    pop rbp
+    ret
